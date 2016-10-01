421f62c187f73844e9880f5cd4f71695a40375b8
Casey Gollan <casey@bullshit.systems>
2016-10-01 19:56:40 -0400

    fixes publication image sizes

--- site/templates/publications.php
+++ site/templates/publications.php
@@ -7,10 +7,15 @@
 </div>
 <div class="row">
   <?php foreach ($page->children() as $publication): ?>
-    <a class="col-xs-6 col-sm-4 col-md-3" href="<?php echo $publication->url() ?>">
-      <img class="img-fluid" src="<?php echo $publication->images()->first()->url() ?>" alt="<?php echo $publication->title() ?>" />
-    </a>
+    <?php if ($publication->hasImages()): ?>
+      <div class="col-xs-6 col-sm-4 col-md-3">
+        <a class="text-hide publication-link" href="<?php echo $publication->url() ?>" style="background-image:url('<?php echo $publication->images()->first()->resize(255, 255)->url() ?>')" alt="<?php echo $publication->title() ?>"><?php echo $publication->title() ?></a>
+      </div>
+    <?php else: ?>
+      <div class="col-xs-6 col-sm-4 col-md-3">
+        <a class="publication-link" href="<?php echo $publication->url() ?>" alt="<?php echo $publication->title() ?>"><?php echo $publication->title() ?></a>
+      </div>
+    <?php endif; ?>
   <?php endforeach; ?>
 </div>
-
 <?php snippet('footer') ?>
