f28766a187aa3b4fa9902e6f7cf3c9614a0962eb
Casey Gollan <casey@bullshit.systems>
2016-11-05 09:59:41 -0400

    require and filter artists, fixes #35

--- site/templates/newses.php
+++ site/templates/newses.php
@@ -5,7 +5,7 @@
     <h3 class="heading-small m-b-2"><a href="/news">Recent News</a></h3>
     <ul class="list-unstyled artists-list">
       <?php
-      $artists = $page->children()->visible()->groupBy('artists');
+      $artists = $page->children()->visible()->filterBy('artists', '!=', '')->groupBy('artists');
       foreach($artists as $artist => $items):
       $artist = $pages->find('artists')->children()->visible()->find($artist); ?>
         <li><a <?php if (kirby()->request()->params()->artist() == $artist->slug()): ?>class="font-weight-bold"<?php endif; ?> href="/news/artist:<?php echo $artist->slug() ?>"><?php echo $artist->first_name() . " " . $artist->last_name() ?></a></li>
