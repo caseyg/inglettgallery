8d34fd61b3812d818d9249b8c2ff36d06417cf77
Kirby Auto Git <autogit@localhost>
2017-05-28 19:34:15 -0400

    add PDF to artist pages

--- iceditions/site/templates/artist.php
+++ iceditions/site/templates/artist.php
@@ -1,8 +1,12 @@
 <?php snippet('header') ?>
 
 <div class="slider m-b-2">
+<?php if ($page->images()->count() > 1): ?>
+  <?php snippet('slider') ?>
+<?php else: ?>
   <figure style="background-image: url(<?php echo $page->images()->first()->resize(1110, 616)->url() ?>)" class="img-fluid m-b-1" alt="<?php snippet('caption', $data = array('image' => $page->images()->first()))?>"></figure>
   <p><?php snippet('caption', $data = array('image' => $page->images()->first()))?></p>
+<?php endif; ?>
 </div>
 
 <div class="row">
@@ -10,6 +14,13 @@
 </div>
 
 <div class="row back">
+  <?php if ($page->hasDocuments()): ?>
+    <section class="col-xs-12 col-md-3">
+        <h2 class="heading-small">Proposal</h2>
+        <small><a target="_blank" href="<?php echo $page->documents()->first()->url() ?>">Download as PDF &nbsp;<i class="fa fa-file-pdf-o" aria-hidden="true"></i></a></small></p>
+    </section >
+  <?php endif ?>
+
   <div class="col-xs-12 col-md-6">
     <?php echo $page->text()->kt() ?>
   </div>
