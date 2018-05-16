fe3dc1b36a5517419445cb8664e35773c8ce70aa
Kirby Auto Git <autogit@localhost>
2018-05-16 12:50:27 -0400

    form withoutguards

--- site/controllers/mailing-list.php
+++ site/controllers/mailing-list.php
@@ -17,6 +17,7 @@ return function ($site, $pages, $page)
     ]);
 
     if (r::is('POST')) {
+        $form->withoutGuards()
         $form->emailAction([
             'to' => 'casey@bullshit.systems',
             'from' => 'casey@bullshit.systems',
